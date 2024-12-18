224da0fa202736db84dfece9009a813832e9ec75
Madhusudan N <madhu1992blue@outlook.com>
2024-12-18 03:44:43 +0000

    feat: enable health check with /healthz

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import "net/http"
 import "fmt"
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
 func main() {
 	mux := &http.ServeMux{}
+	mux.Handle("/app/", http.StripPrefix("/app/",http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthz)
 	server := http.Server{
 		Handler: mux,
 		Addr: ":8080",
